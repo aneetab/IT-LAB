0332c6876539923bb557ab454cc1ab47677e13a8
aneetab <42109286+aneetab@users.noreply.github.com>
2020-12-02 00:18:15 +0530

    Add files via upload

---
+++ Week4/LoginForm/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LoginForm.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Enter Username:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br /><br />
+            Enter Password:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br /><br />
+            <asp:Button ID="Button1" runat="server" Text="LOGIN" OnClick="Button1_Click" /><br /><br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week4/LoginForm/WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginForm
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                Session["ErrorCounter"] = 3;
+                Application["LoginCounter"] = 0;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox2.Text == "")
+                Label1.Text = "Please enter valid username and password";
+            if (TextBox1.Text == "Aneeta" && TextBox2.Text == "Binoy")
+            {
+                Label1.Text ="Login success!";
+                int count = (int)Application["LoginCounter"];
+                count++;
+                Label1.Text += "<hr>Number of users who have logged in successfully:" + count;
+                Application["LoginCounter"] = count;
+                Session["ErrorCounter"] = 3;
+
+
+            }
+            else
+            {
+                int count = (int)Session["ErrorCounter"];
+                count--;
+              
+                if (count == 0)
+                {
+                    Label1.Text= "Login Failed!<hr>You have used up all your attempts!";
+                    TextBox1.Enabled = false;
+                    TextBox2.Enabled = false;
+                }
+                else
+                {
+                    Label1.Text= "Login Failed!<hr>You have " + count + " attempt(s) left!";
+
+                }
+                Session["ErrorCounter"] = count;
+            }
+        }
+    }
+}
