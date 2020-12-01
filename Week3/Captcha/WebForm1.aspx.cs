c267534a8818481b6f11d30652c9633b766d3b58
aneetab <42109286+aneetab@users.noreply.github.com>
2020-12-01 23:19:38 +0530

    Add files via upload

---
+++ Week3/Captcha/WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Captcha
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static int count=0;
+        string captcha = "LHGFHS";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if(!this.IsPostBack)
+            {
+                count = 3; 
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            count--;
+            string inp = TextBox1.Text;
+            if(string.Equals(captcha,inp))
+            {
+                Label2.Text = "Success!";
+                count = 3;
+            }
+            else if(count==0)
+            {
+                Label2.Text = "Incorrect Captcha!";
+                TextBox1.Enabled = false;
+            }
+            else
+            {
+                Label2.Text = "Incorrect Captcha!You've got "+count+"more attempts.";
+            }
+            
+
+        }
+    }
+}
