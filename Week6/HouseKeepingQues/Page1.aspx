b04c6103d70a48d1a6daa341662101d62e057b92
aneetab <42109286+aneetab@users.noreply.github.com>
2020-12-02 22:40:36 +0530

    Add files via upload
    Asked to display details in label, I've dsiplayed details in a gridview instead

---
+++ Week6/HouseKeepingQues/Page1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Page1.aspx.cs" Inherits="HouseKeepingQues.Page1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Select the ID of the staff whose details you want to display:<br />
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList><br /><br />
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView><br /><br />
+            Select a city to update city:<br />
+            <asp:ListBox ID="ListBox1" runat="server"></asp:ListBox><br /><br />
+            <asp:Button ID="Button1" runat="server" Text="Update" OnClick="Button1_Click" /><br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week6/HouseKeepingQues/Page1.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HouseKeepingQues
+{
+
+    public partial class Page1 : System.Web.UI.Page
+    {
+        string connectionString = WebConfigurationManager.ConnectionStrings["HouseKeeping"].ConnectionString;
+        DataSet ds = new DataSet();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!this.IsPostBack)
+            {
+                SqlConnection myConnection = new SqlConnection(connectionString);
+                try
+                {
+                    DropDownList1.Items.Add("");
+                    myConnection.Open();
+                    SqlCommand command = new SqlCommand("Select DISTINCT StaffID from Staff", myConnection);
+                   // SqlDataReader reader;
+                    //reader = command.ExecuteReader();
+                    //while(reader.Read())
+                    //{
+                      //  DropDownList1.Items.Add(reader["StaffID"].ToString());
+                    //}
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(ds, "staff_ids");
+                    DropDownList1.DataSource = ds.Tables["staff_ids"];
+                    DropDownList1.DataTextField = "StaffID";
+                  //  DropDownList1.DataValueField = "StaffID";
+                    DropDownList1.DataBind();
+                    
+                    //DropDownList1.DataBind();
+
+              
+                }
+                catch (Exception err)
+                {
+                    Label1.Text = "Error reaing the database";
+                    Label1.Text += err.Message;
+                }
+                finally
+                {
+                    myConnection.Close();
+                  
+                }
+
+
+                ListBox1.Items.Add("Bangalore");
+                ListBox1.Items.Add("Chennai");
+                ListBox1.Items.Add("Hyderabad");
+                ListBox1.Items.Add("kolkata");
+                ListBox1.Items.Add("Delhi");
+                ListBox1.Items.Add("Nagpur");
+            }
+            
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand("Select FirstName,LastName,DNo,Street,City,State,ZipCode from Staff where StaffID=@staffid", myConnection);
+            command.Parameters.AddWithValue("@staffid", DropDownList1.SelectedItem.Text);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            try
+            {
+                myConnection.Open();
+                adapter.Fill(ds, "staff_details");
+                GridView1.DataSource = ds.Tables["staff_details"];
+                GridView1.DataBind();
+            }
+            catch(Exception err)
+            {
+                Label1.Text = "Error reading details";
+                Label1.Text += err.Message;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedIndex == 0)
+            {
+                Label1.Text = "You need to select an staff";
+            }
+           else if (ListBox1.SelectedIndex==-1)
+            {
+                Label1.Text = "You need to select new city to update";
+            }
+            else
+            {
+                SqlConnection myConnection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("Update Staff set City=@city where StaffID=@staffid", myConnection);
+                command.Parameters.AddWithValue("city", ListBox1.SelectedItem.Text);
+                command.Parameters.AddWithValue("staffid", DropDownList1.SelectedItem.Text);
+                try
+                {
+                    myConnection.Open();
+                    int updated = command.ExecuteNonQuery();
+                    Label1.Text = updated.ToString() + "record(s) updated";
+                }
+                catch(Exception err)
+                {
+                    Label1.Text = "Error updating table";
+                    Label1.Text += err.Message;
+                }
+                finally
+                {
+                    myConnection.Close();
+                }
+                
+            }
+           
+        }
+
+        
+    }
+}
