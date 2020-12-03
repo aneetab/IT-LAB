d191e38f4c39be9fcabe4a2922d751115907f6d4
aneetab <42109286+aneetab@users.noreply.github.com>
2020-12-03 12:25:33 +0530

    Add files via upload

---
+++ Week3/EmployeePromotion/Ques2.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ques2.aspx.cs" Inherits="EmployeePromotion.Ques2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+              <asp:Panel ID="Panel1" runat="server" Height="200px" Width="330px" style="margin-left:500px;margin-top:50px;">
+                  <asp:Image ID="Image1" Height="50%" Width="50%" runat="server" />
+              </asp:Panel>
+        <div>
+            Select Employee ID: <br />
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList><br /><br />
+            Name of employee:<br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br /><br />
+            Enter DOJ(MM/DD/YYYY): <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+            <asp:Label ID="Label2" runat="server" Text=""></asp:Label><br /><br />
+            <asp:Button ID="Button1" runat="server" Text="Am I Eligible for Promotion" OnClick="Button1_Click" /><br /><br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            
+        </div>
+  
+    </form>
+</body>
+</html>
---
+++ Week3/EmployeePromotion/Ques2.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmployeePromotion
+{
+    public partial class Ques2 : System.Web.UI.Page
+    {
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+       
+        
+        
+        
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+            if (!this.IsPostBack)
+            {
+                DropDownList1.Items.Add(new ListItem("---", "Select an ID"));
+                DropDownList1.Items.Add(new ListItem("123-01", "Claire M.Dunphy"));
+                DropDownList1.Items.Add(new ListItem("123-02", "Jim Halpert"));
+                DropDownList1.Items.Add(new ListItem("123-03", "Michael Scott"));
+                DropDownList1.Items.Add(new ListItem("123-04", "Pam Beesly"));
+                dic.Add("123-01", "Images/claire.png");
+                dic.Add("123-02", "Images/jim.png");
+                dic.Add("123-03", "Images/michael.png");
+                dic.Add("123-04", "Images/pam.jpg");
+
+
+            }
+            else
+            {
+                dic.Add("000", "");
+                dic.Add("123-01", "Images/claire.png");
+                dic.Add("123-02", "Images/jim.png");
+                dic.Add("123-03", "Images/michael.png");
+                dic.Add("123-04", "Images/pam.png");
+            }
+            Label2.Text = "";
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(TextBox2.Text!="")
+            {
+                try
+                {
+                    DateTime mydate = DateTime.Parse(TextBox2.Text);
+                    DateTime current = DateTime.Now;
+                    TimeSpan t = current - mydate;
+                    double days = t.TotalDays;
+                    double check = 5 * 365.0;
+                    int years = (int)days / 365;
+                    if(days>=check)
+                    {
+                        Label1.Text = "You are eligible for a promotion as you have worked for " + years + " years";
+
+                    }
+                    else
+                    {
+                        Label1.Text = "You are not eligible for a promotion as you have worked for only " + years + " years";
+
+                    }
+                   // Label1.Text = mydate.ToString();
+
+
+                }
+                catch
+                {
+                    Label2.Text = "Please enter date in specified format";
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string ID = DropDownList1.SelectedItem.ToString();
+            TextBox1.ReadOnly = true;
+            if (ID == "---")
+            {
+                TextBox1.Text = "";
+            }
+            else
+            {
+                TextBox1.Text = DropDownList1.SelectedItem.Value;
+                Image1.ImageUrl = dic[ID];
+            }
+            
+           
+
+            
+         
+
+        }
+    }
+}
