01eb7d9eac07ec2e4790fe7e2666ec8b2e66170b
aneetab <42109286+aneetab@users.noreply.github.com>
2020-12-04 14:52:18 +0530

    Add files via upload

---
+++ Week5/QUES3/default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Ques3._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server"></asp:Image><br /><br />
+            <asp:Label ID="Label1" runat="server" Text="Select a theme:"></asp:Label><br /><br />
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList><br /><br />
+            <asp:TextBox ID="TextBox1" runat="server" Text="Hello World" ReadOnly="true"></asp:TextBox><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week5/QUES3/default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ques3
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!this.IsPostBack)
+            {
+                DropDownList1.Items.Add("");
+                DropDownList1.Items.Add("Summer");
+                DropDownList1.Items.Add("Monsoon");
+                if(Session["Theme"]!=null)
+                {
+                    DropDownList1.Items.FindByText(Session["Theme"].ToString()).Selected = true;
+                }
+            }
+
+        }
+        protected void Page_PreInit(object sender,EventArgs e)
+        {
+            if(Session["Theme"]!=null)
+            {
+                Page.Theme = Session["Theme"].ToString();
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Theme"] = DropDownList1.SelectedValue;
+            Response.Redirect(Request.FilePath);
+        }
+    }
+}
