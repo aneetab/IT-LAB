ea5fff924cdb47ae51384070ea4f84269143578c
aneetab <42109286+aneetab@users.noreply.github.com>
2020-12-08 00:04:54 +0530

    Add files via upload

---
+++ Week7/QUES3/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="QUES3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             Select a state:<br />
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true"></asp:DropDownList><br />
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ConnectionStrings:States %>" SelectCommand="Select * from City where StateID=@id">
+                <SelectParameters>
+                    <asp:ControlParameter Name="id" ControlID="DropDownList1" PropertyName="SelectedValue"  />
+                </SelectParameters>
+            </asp:SqlDataSource>
+            <asp:DropDownList ID="DropDownList2" runat="server" DataSourceID="SqlDataSource1" DataTextField="CityName" AutoPostBack="true" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged"></asp:DropDownList><br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week7/QUES3/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QUES3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+
+        
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                Dictionary<int, string> dic = new Dictionary<int, string>();
+                dic.Add(1, "Maharashtra");
+                dic.Add(2, "Kerala");
+                dic.Add(3, "Karnataka");
+                dic.Add(4, "Uttar Pradesh");
+                dic.Add(5, "West Bengal");
+                DropDownList1.DataSource = dic;
+                DropDownList1.DataTextField = "Value";
+                DropDownList1.DataValueField = "Key";
+                
+                this.DataBind();
+            }
+
+           
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label1.Text = "Selected Details:<hr>";
+            Label1.Text += "State:" + DropDownList1.SelectedItem.Text;
+            Label1.Text += "<hr>City:" + DropDownList2.SelectedItem.Text;
+        }
+    }
+}
