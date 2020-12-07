4517ec7c37e23f40e07320b854df35fd382b0c39
aneetab <42109286+aneetab@users.noreply.github.com>
2020-12-07 23:58:49 +0530

    Add files via upload

---
+++ Week7/QUES1/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="QUES1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Choose a fruit:<br />
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server"></asp:CheckBoxList><br />
+            Choose an icecream:<br />
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server"></asp:RadioButtonList><br />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" /><hr />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Week7/QUES1/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QUES1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!this.IsPostBack)
+            {
+                List<string> fruits = new List<string>();
+                fruits.Add("Apple");
+                fruits.Add("Mango");
+                fruits.Add("Banana");
+                fruits.Add("Chikku");
+                fruits.Add("Litchi");
+                fruits.Add("Pineapple");
+                List<ListItem> icrecream = new List<ListItem>();
+                icrecream.Add(new ListItem("Butterscotch", "150.00"));
+                icrecream.Add(new ListItem("Vanilla", "100.00"));
+                icrecream.Add(new ListItem("Strawberry", "120.00"));
+                CheckBoxList1.DataSource = fruits;
+                RadioButtonList1.DataSource = icrecream;
+                RadioButtonList1.DataTextField = "Text";
+                RadioButtonList1.DataValueField = "Value";
+                this.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(CheckBoxList1.SelectedIndex==-1||RadioButtonList1.SelectedIndex==-1)
+            {
+                Label1.Text += "Please slect a fruit and icecream flavour!";
+            }
+            else
+            {
+                Label1.Text = "Selected details are:<hr>";
+                Label1.Text += "Fruit:" + CheckBoxList1.SelectedItem.Text;
+                Label1.Text += "<hr>IceCream Flavour:" + RadioButtonList1.SelectedItem.Text;
+                Label1.Text += "<hr>Price:" + RadioButtonList1.SelectedItem.Value;
+            }
+        }
+    }
+}
